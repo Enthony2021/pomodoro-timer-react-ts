0ddce1a1649cf0d2eca43b42b246e57db8f20ee7
Enthony Stevie <enthonyoliveira96@gmail.com>
2023-04-15 23:34:21 -0700

    instalação de dependências e início do pomodoro-timer component

---
+++ src/components/pomodoro-timer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useInterval } from '../hooks/use-interval';
+
+interface Props {
+  defaultPomodoroTime: number;
+}
+
+export function PomodoroTimer(props: Props) {
+  const [mainTime, setMainTime] = React.useState(props.defaultPomodoroTime);
+
+  useInterval(() => {
+    setMainTime(mainTime - 1);
+  }, 1000);
+  return <div>Olá Mundo! {mainTime}</div>;
+}
