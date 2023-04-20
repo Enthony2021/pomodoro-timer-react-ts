f580822bd294401cbbf089cf2fd01ee2132974af
Enthony Stevie <89668958+Enthony2021@users.noreply.github.com>
2023-04-19 22:48:33 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="container">
       <PomodoroTimer
-        pomodoroTime={10}
-        shortRestTime={2}
-        longRestTime={4}
+        pomodoroTime={1500}
+        shortRestTime={300}
+        longRestTime={900}
         cycles={4}
       />
     </div>
